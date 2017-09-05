f2bd082805b45567ed5cd254451aaebab6783c1e
Nic Schumann <nicolas_schumann@brown.edu>
2017-09-05 17:30:49 -0400

    [Config] fixed an issue in the deploy script

--- .deploy.sh
+++ .deploy.sh
@@ -4,7 +4,7 @@ npm run build
 
 source ./.env
 
-scp -r ./dist/wp-content/themes/custom root@$DROPLET_IP:/var/www/html/wp-content/themes/custom
+scp -r ./dist/wp-content/themes/custom root@$DROPLET_IP:/var/www/html/wp-content/themes
 scp -r ./dist/wp-content/plugins root@$DROPLET_IP:/var/www/html/wp-content/
 scp -r ./dist/wp-content/uploads root@$DROPLET_IP:/var/www/html/wp-content/
 
