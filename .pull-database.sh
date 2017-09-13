ff740e8a97ce136e7e2266a842ad4b73c210ff44
Nic Schumann <nicolas_schumann@brown.edu>
2017-09-13 10:55:50 -0400

    [Config] added a simple script for importing a WP database into a docker instance.

--- .pull-database.sh
+++ .pull-database.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "pull-database not implemented"
+source ./.env
+
+MIGRATION_MINUS_1_NAME="migration-$(date +%s).old"
+MIGRATION_NAME="migration.sql"
+
+mkdir -p ./migrations
+docker exec $DOCKER_DATABASE_CONTAINER mysqldump -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD $WORDPRESS_DB_NAME > ./migrations/$MIGRATION_MINUS_1_NAME
+docker exec -i $DOCKER_DATABASE_CONTAINER mysql -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD $WORDPRESS_DB_NAME < $1
+# docker exec $DOCKER_DATABASE_CONTAINER rm /root/$MIGRATION_NAME
+# docker exec $DOCKER_DATABASE_CONTAINER ls /root
