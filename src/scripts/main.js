656dede4301a6767c5d7d7ec5cc849421ed9f4f9
Nic Schumann <nicolas_schumann@brown.edu>
2017-09-13 15:21:34 -0400

    [Scripts] added a toggle-class script.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 "use strict";
 
-console.log('loaded');
+import { toggleClass } from "./toggle-class.js";
+
+console.log('main.js loaded');
+
+toggleClass({ selector: "#sidebar", action: "click", toggledClass: "open" });
---
+++ src/scripts/toggle-class.js
@@ -0,0 +1,14 @@
+function toggleClass( config ) {
+    console.log('toggle-class.js loaded');
+
+    $(document).ready( function() {
+        $( config.selector ).on( config.action, function() {
+
+            $(this).toggleClass( config.toggledClass );
+
+        });
+    });
+
+}
+
+export { toggleClass };
