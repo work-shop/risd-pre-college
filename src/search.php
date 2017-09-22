77c01e8a91ff680448a86e9bebca69f7fd289714
Nic Schumann <nicolas_schumann@brown.edu>
2017-09-22 15:47:05 -0400

    [Search] implemented search logic and styling.

--- src/search.php
+++ src/search.php
@@ -1,1 +1,20 @@
 <?php
+/**
+ * Template Name: Search Page
+ */
+global $query_string;
+
+wp_parse_str( $query_string, $parameters );
+
+$parameters["post_type"] = array("page");
+$parameters["posts_per_page"] = -1;
+
+$context = Timber::get_context();
+$posts = Timber::get_posts( $parameters );
+
+$context['query'] = $parameters['s'];
+$context['results'] = $posts;
+
+Timber::render( array('search.twig'), $context );
+
+?>
